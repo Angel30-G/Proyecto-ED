07fda495c5c4f475dbc8b00730cef1c35886234d
Yess Solano <90736001+yessmsr@users.noreply.github.com>
2022-05-13 21:52:39 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<fstream>
+#include <windows.h>
 #include "Trie.h"
+#include "DLinkedList.h"
+
 using namespace std;
 
-int main()
-{
+int main(){
+    setlocale(LC_ALL, "spanish");
+    SetConsoleCP(1252);
+    SetConsoleOutputCP(1252);
+
     Trie t;
     t.insert("palabra");
     t.insert("padre");
-    t.insert("padrón");
+    t.insert("padrÃ³n");
     t.insert("padrino");
     t.insert("pala");
     t.insert("palanca");
     t.insert("poeta");
     List<string> *palabras = t.getMatches("pa");
     palabras->print();
+    t.lectura();
     delete palabras;
+
+
     return 0;
 }
+
